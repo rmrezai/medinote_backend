c8afef9a4ebf1bd47471dbe7fb45ecf576a5d625
rmrezai <36303431+rmrezai@users.noreply.github.com>
2025-08-14 07:18:17 -0400

    Add CORS middleware configuration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+
 from app.hoop_engine import hoop_engine
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
